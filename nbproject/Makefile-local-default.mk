30c55086f0ea72d623266a8dc5c98f9853fcc21f
andrew-jacobs <andrew.jacobs@btinternet.com>
2018-04-02 16:26:57 +0100

    Revised schematic for new board

--- nbproject/Makefile-local-default.mk
+++ nbproject/Makefile-local-default.mk
@@ -15,23 +15,23 @@
 # $ makeMP_CC="/opt/microchip/mplabc30/v3.30c/bin/pic30-gcc" ...  
 #
 SHELL=cmd.exe
-PATH_TO_IDE_BIN=C:/Program Files (x86)/Microchip/MPLABX/v3.45/mplab_ide/platform/../mplab_ide/modules/../../bin/
+PATH_TO_IDE_BIN=C:/Program Files (x86)/Microchip/MPLABX/v4.05/mplab_ide/platform/../mplab_ide/modules/../../bin/
 # Adding MPLAB X bin directory to path.
-PATH:=C:/Program Files (x86)/Microchip/MPLABX/v3.45/mplab_ide/platform/../mplab_ide/modules/../../bin/:$(PATH)
+PATH:=C:/Program Files (x86)/Microchip/MPLABX/v4.05/mplab_ide/platform/../mplab_ide/modules/../../bin/:$(PATH)
 # Path to java used to run MPLAB X when this makefile was created
-MP_JAVA_PATH="C:\Program Files (x86)\Microchip\MPLABX\v3.45\sys\java\jre1.8.0_91/bin/"
+MP_JAVA_PATH="C:\Program Files (x86)\Microchip\MPLABX\v4.05\sys\java\jre1.8.0_144/bin/"
 OS_CURRENT="$(shell uname -s)"
 # MP_CC is not defined
 # MP_CPPC is not defined
 # MP_BC is not defined
-MP_AS="C:\Program Files (x86)\Microchip\MPLABX\v3.45\mpasmx\mpasmx.exe"
-MP_LD="C:\Program Files (x86)\Microchip\MPLABX\v3.45\mpasmx\mplink.exe"
-MP_AR="C:\Program Files (x86)\Microchip\MPLABX\v3.45\mpasmx\mplib.exe"
-DEP_GEN=${MP_JAVA_PATH}java -jar "C:/Program Files (x86)/Microchip/MPLABX/v3.45/mplab_ide/platform/../mplab_ide/modules/../../bin/extractobjectdependencies.jar"
+MP_AS="C:\Program Files (x86)\Microchip\MPLABX\v4.05\mpasmx\mpasmx.exe"
+MP_LD="C:\Program Files (x86)\Microchip\MPLABX\v4.05\mpasmx\mplink.exe"
+MP_AR="C:\Program Files (x86)\Microchip\MPLABX\v4.05\mpasmx\mplib.exe"
+DEP_GEN=${MP_JAVA_PATH}java -jar "C:/Program Files (x86)/Microchip/MPLABX/v4.05/mplab_ide/platform/../mplab_ide/modules/../../bin/extractobjectdependencies.jar"
 # MP_CC_DIR is not defined
 # MP_CPPC_DIR is not defined
 # MP_BC_DIR is not defined
-MP_AS_DIR="C:\Program Files (x86)\Microchip\MPLABX\v3.45\mpasmx"
-MP_LD_DIR="C:\Program Files (x86)\Microchip\MPLABX\v3.45\mpasmx"
-MP_AR_DIR="C:\Program Files (x86)\Microchip\MPLABX\v3.45\mpasmx"
+MP_AS_DIR="C:\Program Files (x86)\Microchip\MPLABX\v4.05\mpasmx"
+MP_LD_DIR="C:\Program Files (x86)\Microchip\MPLABX\v4.05\mpasmx"
+MP_AR_DIR="C:\Program Files (x86)\Microchip\MPLABX\v4.05\mpasmx"
 # MP_BC_DIR is not defined
